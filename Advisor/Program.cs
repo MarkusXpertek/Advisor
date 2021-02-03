6f2f259750de5e831e5e3ded79df709cefedb2b6
Markus Madeleyn <Markus.Madeleyn@xpertek.co.za>
2021-02-03 11:39:05 +0200

    Add project files.

---
+++ Advisor/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CommonLibrary;
+
+namespace Advisor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Xpert xpert = new Xpert();
+            Console.WriteLine(xpert.Advice());
+        }
+    }
+}
